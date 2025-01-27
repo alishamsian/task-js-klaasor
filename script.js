ac13883f32f86d6fd55706014d9333665d7b1998
Ali Shamsian <84272102+alishamsian@users.noreply.github.com>
2025-01-27 19:15:12 +0330

    end number is prime

---
+++ script.js
@@ -0,0 +1,18 @@
+// 1.Create a function that checks if a number is prime
+
+function isPrime(number) {
+  if (number <= 1) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(isPrime(7))
+
+
+// Math.sqrt()
