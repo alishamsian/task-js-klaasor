85622e9adbfea8641a164f0c6a4d5edf504d5079
Ali Shamsian <84272102+alishamsian@users.noreply.github.com>
2025-01-27 19:46:07 +0330

    end Create a function that converts time from 24-hour format to 12-hour format.

--- script.js
+++ script.js
@@ -14,5 +14,28 @@ function isPrime(number) {
 
 console.log(isPrime(7))
 
+// new ==> Math.sqrt()
 
-// Math.sqrt()
+
+// Create a function that converts time from 24-hour format to 12-hour format.
+
+function convert24to12(time){
+  let parts = time.split(':');
+  let hour = parseInt(parts[0] , 10);
+  let minute = parts[1]
+  let period = time <= 2 ? "PM" : "AM"
+
+  if(hour === 0){
+    hour = 12
+  }else if (hour > 12){
+    hour -= 12
+  }
+
+  return hour + ":" + minute + " " + period;
+}
+
+console.log(convert24to12("13:43"))
+
+// new ==> split , parseInt
+
+
